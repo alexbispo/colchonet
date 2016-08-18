f3e80cf3aa05847d91b80c7d66e58dec62bf6c0b
alexbispo <alexbispo.artes@gmail.com>
2016-08-18 16:06:28 -0300

    (7.3) Alterando o idioma do site.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  LOCALES = /en|pt\-BR/
+
+  scope "(:locale)", locale: LOCALES do 
+    resources :rooms
+    resources :users
+  end
+
+  get '/:locale' => 'home#index', locale: LOCALES
   root 'home#index'
-  resources :rooms
-  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
